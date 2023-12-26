11d033bf7212bbbb861c41fc7f2d5af6010edda4
Floodnut <gsniper777@gmail.com>
2023-12-26 23:56:49 +0900

    feat: init basic-lambda execution role

--- src/lambda/iam.tf
+++ src/lambda/iam.tf
@@ -1,3 +1,8 @@
 resource "aws_iam_role" "lambda_role" {
     // Todo: IAM Role for Lambda
 }
+
+resource "aws_iam_role_policy_attachment" "basic-execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
